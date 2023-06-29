12818de91f9d9349ccc3f8528b2ee2fad0906381
Ramael <ramael.bruno@gmail.com>
2023-06-29 11:16:51 +0200

    creation of the project webfactory backend and set libraries and server

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+//connexion to database
+mongoose.connect(process.env.DATABASE_ACCESS,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+  const app = express();
+  app.use(express.json());
+
+  //give access
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+//connexion to routes
+
+  module.exports = app;
